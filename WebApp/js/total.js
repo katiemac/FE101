c492998ffbc1a93f741253be83b281da8c763957
katiemac <kmaclachlan@hearst.com>
2014-07-03 21:28:30 -0400

    making the most important parts work

--- WebApp/js/total.js
+++ WebApp/js/total.js
@@ -84,7 +84,7 @@ Caman.Filter.register("bright", function(grey) {
     //close modal --- need to add saving out the canvas and adding to the other canvas
     $(" #done ").click(function(){ 
         //grab the context from your destination canvas
-        var destCtx = document.getElementById('canvas2').getContext('2d');
+        var destCtx = document.getElementById('canvas').getContext('2d');
         destCtx.drawImage(document.getElementById('preset-example'), 0, 0);    
         $("#imageEdit").hide();
     });
@@ -95,12 +95,71 @@ Caman.Filter.register("bright", function(grey) {
 
 }).call(this);
 
+
 /*------------------------------------------------------------------------------
-         Save and Add to Canvas
+        to add text from input on to the canvas 
+------------------------------------------------------------------------------*/
+  // adds the text in canvas, on multiple lines
+  // ctx = reference to canvas context
+  // text = the text to add
+  // x = horizontal position where to start adding the text
+  // y = vertical position where to start adding the text
+  // maxWidth = the maximum width of the text line
+  // lineHeight = the height of the line
+
+  // sets maximum line width, line height, and x /y coords for text
+  var cnv1 = document.getElementById('canvas');
+  var ctx1 = cnv1.getContext('2d');
+  var maxWidth = cnv1.width - 10;
+  var lineHeight = 23;
+  var x_pos = (cnv1.width - maxWidth) / 2;
+  var y_pos = 15;
+
+  function  addTextCnv(ctx, text, x, y, maxWidth, lineHeight) {
+  
+  if (!text){
+    return;
+  } //exits the function if there is no text
+
+  // splits the text in words to can wrap it on new lie if exceds maxWidth
+    var words = text.split(' ');
+    var nr_w = words.length;
+    var addtxt = '';
+
+    // sets to add the text and rows
+    for(var n = 0; n < nr_w; n++) {
+      var txtLine = addtxt + words[n] + ' ';
+      var metrics = ctx.measureText(txtLine);
+      var txtWidth = metrics.width;
+      if (txtWidth > maxWidth && n > 0) {
+        ctx.fillText(addtxt, x, y);
+        addtxt = words[n] + ' ';
+        y += lineHeight;
+      }
+      else {addtxt = txtLine;}
+      }
+
+    // adds the text in canvas (sets text color, font type and size)
+    ctx.fillStyle = '#2ac498';
+    ctx.font = 'bold 17px sans-serif';
+    ctx.fillText(addtxt, x, y);
+  }
+
+
+/*------------------------------------------------------------------------------
+          UPDATE THE CANVAS 
 ------------------------------------------------------------------------------*/
 
+  function update(){
+    //clearCanvas(cnv1); // clears the canvas
+    //add the styles based on the dropdown
+    addTextCnv(ctx1, this.value, x_pos, y_pos, maxWidth, lineHeight); // adds the text
+    //Caman.reloadCanvasData();
+  }
+  
+  update();
 
-
-
+  // register onkeyup event for #text_cnv text field to repaint the canvas
+  document.getElementById('text_cnv').onkeyup = update;
 
 
